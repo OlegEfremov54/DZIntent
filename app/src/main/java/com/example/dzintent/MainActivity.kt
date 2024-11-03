fd35aeeaa02d04ee3026ca8d2d362e6653509561
OlegEfremov54 <ole_f@mail.ru>
2024-11-03 15:36:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dzintent/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.dzintent
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbarMain: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        toolbarMain = findViewById(R.id.toolbarMain)
+        setSupportActionBar(toolbarMain)
+        title = "ДЗ Скрытый калькулятор"
+        toolbarMain.subtitle = "Версия 1."
+        toolbarMain.setLogo(R.drawable.ic_android_black_24dp)
+
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.infoMenuMain -> {
+                Toast.makeText(applicationContext, "Автор Ефремов О.В. Версия 1.1.",
+                    Toast.LENGTH_LONG).show()
+            }
+            R.id.exitMenuMain ->{
+                Toast.makeText(applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
